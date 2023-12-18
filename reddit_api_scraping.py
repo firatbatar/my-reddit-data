8bfe6561e0558f002b885219d2dcbb38938c7829
Fırat Batar <firat.o.batar@gmail.com>
2023-12-18 10:28:27 +0300

    Add latest figures to site branch (#2)
    * Remove voted posts flairs
    Submissions don't have flairs mostly, so I will not use them.
    
    * Scrapper script combines annotated tags
    
    * Bug fix
    
    * Rename tag file
    
    * Update
    
    * Cont'd
    
    * Final data collected
    
    * Remove unused API calls
    
    * Seperate processing, visualization, and analysis notebooks
    
    * Change figures
    
    * Forgot to commit visualization notebook
    
    * Remove old notebook
    
    * Analysis cont'd
    
    * Analysis cont'd
    
    * Finish (roughly) the analysis
    
    * Filter data more
    Remove any IP log before October 2023 to analyze this school year usage.
    
    * Change subcount default min
    
    * Fix label on subbed
    
    * Reverse colors in grouped charts

--- reddit_api_scraping.py
+++ reddit_api_scraping.py
@@ -1,34 +1,14 @@
-def get_submission_vote_count(reddit, submission_id):
+def get_submission_score(reddit, submission_id):
     from math import ceil
     from numpy import nan as np_nan
 
     try:
         submission = reddit.submission(id=submission_id)
         submission_score = submission.score
-        submission_upvote_ratio = submission.upvote_ratio
     except:
-        return [np_nan, np_nan]  # Return NaN if the submission is hidden or inaccessible for some reason
+        return np_nan  # Return NaN if the submission is hidden or inaccessible for some reason
 
-    # Calculate the number of upvotes and downvotes
-    if submission_upvote_ratio == 1:
-        return [submission_score, 0]
-    if submission_upvote_ratio == 0.5:
-        return [submission_score / 2, submission_score / 2]
-
-    downvotes = submission_score * (submission_upvote_ratio - 1) / (-2 * submission_upvote_ratio + 1)
-    upvotes = -submission_upvote_ratio * downvotes / (submission_upvote_ratio - 1)
-
-    return [ceil(upvotes), ceil(downvotes)]
-
-
-def get_submission_flair(reddit, submission_id):
-    try:
-        submission = reddit.submission(id=submission_id)
-        submission_flair = submission.link_flair_text.lower()
-    except:
-        return None  # Return NaN if the submission is hidden or inaccessible for some reason
-
-    return submission_flair
+    return submission_score
 
 
 def get_comment_score(reddit, comment_id):
@@ -43,46 +23,26 @@ def get_comment_score(reddit, comment_id):
     return comment_score
 
 
-def scrape_post_votes(reddit, ids): 
-    ups = []
-    downs = []
+def scrape_post_scores(reddit, ids): 
+    scores = []
     for id in ids:
-        [up, down] = get_submission_vote_count(reddit, id)
-        print(f"ID: {id} - Upvotes: {up}, Downvotes: {down}")
-        ups.append(up)
-        downs.append(down)
+        score = get_submission_score(reddit, id)
+        print(f"ID: {id} - Score: {score}")
+        scores.append(score)
 
-    return [ups, downs]
-
-
-def scrape_post_flairs(reddit, ids): 
-    flairs = []
-    for id in ids:
-        flair = get_submission_flair(reddit, id)
-        print(f"ID: {id} - Flair: {flair}")
-        flairs.append(flair)
-    
-    return flairs
+    return scores
 
 
-def scrape_post_vote_and_flair(reddit, path, fname, save_path):
+def scrape_post_votes(reddit, path, fname, save_path):
     import pandas as pd
 
     # Read the csv file containing the submission ids for the posts that I have voted
     post_votes = pd.read_csv(path + fname)
 
     # Get votes
-    print("Getting vote counts for posts that I have voted on...")
-    ups, downs = scrape_post_votes(reddit, post_votes["id"])
+    scores = scrape_post_scores(reddit, post_votes["id"])
     # Add the upvotes and downvotes to the dataframe
-    post_votes["Upvotes"] = ups
-    post_votes["Downvotes"] = downs
-
-    # Get flairs
-    print("Getting flairs for posts that I have voted on...")
-    flairs = scrape_post_flairs(reddit, post_votes["id"])
-    # Add the flairs to the dataframe
-    post_votes["Flair"] = flairs
+    post_votes["Scores"] = scores
 
     # Save the dataframe to a csv file
     post_votes.to_csv(save_path + fname, index=False)
@@ -159,18 +119,34 @@ def main():
     raw_data_path = "./data/raw_data/"
     save_path = "./data/scrapped_data/"
 
-    scrape_post_vote_and_flair(reddit, raw_data_path, "post_votes.csv", save_path=save_path)
+    print("Getting vote counts for posts...")
+    scrape_post_votes(reddit, raw_data_path, "post_votes.csv", save_path=save_path)
+    scrape_post_votes(reddit, raw_data_path, "post_headers.csv", save_path=save_path)
 
-    print("Getting vote counts for comments that I have voted on...")
+    print("Getting vote counts for comments...")
     scrape_comment_votes(reddit, raw_data_path, "comment_votes.csv", save_path=save_path)
-
-    scrape_post_vote_and_flair(reddit, raw_data_path, "post_headers.csv", save_path=save_path)
-
-    print("Getting vote counts for the comments that I have created...")
     scrape_comment_votes(reddit, raw_data_path, "comment_headers.csv", save_path=save_path)
 
     print("Getting flairs for the subreddits...")
     get_sub_flairs(reddit, raw_data_path, "subscribed_subreddits.csv", save_path=save_path)
+
+    print("Combining scrapped data with annotated tags...")
+    # Combine annotated tags with scrapped data
+    annotated_data = pd.read_csv(raw_data_path + "tags.csv")
+    scrapped_data = pd.read_csv(save_path + "subscribed_subreddits.csv")
+    scrapped_data["Tags"] = None  # Add a new column for the tags
+
+    # Read the tags for each sub
+    for col in annotated_data.columns[1:]:  # Skip the first column which is the timestamp
+        sub_name = col.split(" ")[0][2:]
+        tags = annotated_data[col].map(lambda x: x.split(", ")).explode().value_counts()
+        accepted_tags = tags[tags >= tags.mean()].sort_values().index.to_list()[:3]
+
+        scrapped_data.loc[scrapped_data["subreddit"] == sub_name, "Tags"] = ", ".join(accepted_tags)
+
+    # Save the dataframe to the csv file
+    scrapped_data.to_csv(save_path + "subscribed_subreddits.csv", index=False)
+
 
 if __name__ == "__main__":
     main()
